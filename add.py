f298302fec49c6b09f151f6dae832013d7a63900
darth-xiaozhijun <15717061273@163.com>
2020-05-31 11:49:53 +0800

    flask数据库用户登录管理

--- add.py
+++ add.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from wtforms import Form, TextField, PasswordField, validators
+import db
 
 app = Flask(__name__)
 
@@ -25,14 +26,24 @@ def add():
     return render_template("add.html")
 
 
-@app.route("/user", methods=["POST","GET"])
+@app.route("/user", methods=["POST", "GET"])
 def user():
     myForm = LoginForm(request.form)
-    if myForm.username.data == "geekshow" and myForm.password.data == "123" and myForm.validate():
-        return redirect("http://www.jikexueyuan.com")
-    else:
-        message = "Login Failed"
-        return render_template("login2.html", message=message, form=myForm)
+    if request.method == "POST":
+        if db.select(myForm.username.data, myForm.password.data):
+            return redirect("http://www.jikexueyuan.com")
+        else:
+            message = "Login Failed"
+            return render_template("login2.html", message=message, form=myForm)
+    return render_template("login2.html", form=myForm)
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    myForm = LoginForm(request.form)
+    if request.method == "POST":
+        db.insert(myForm.username.data, myForm.password.data)
+        return "Register Success"
     return render_template("login2.html", form=myForm)
 
 
--- db.py
+++ db.py
@@ -5,20 +5,37 @@ conn = pymysql.connect("localhost", "root", "123456", "python")
 # 创建游标
 cursor = conn.cursor()
 
-username1 = input("请输入用户名：")
-password1 = input("请输入密码：")
 
-# 插入数据
-# insert_sql = "insert into T_USER(id) values (7)"
-insert_sql = "insert into T_USER(username,password) values ('{0}','{1}')".format(username1,password1)
-print(insert_sql)
-cursor.execute(insert_sql)
-conn.commit();
+def insert(username, password):
+    insert_sql = "insert into T_USER(username,password) values ('{0}','{1}')".format(username, password)
+    cursor.execute(insert_sql)
+    conn.commit();
 
-# 查询数据
-sql = "select * from T_USER"
-cursor.execute(sql)
-result = cursor.fetchall()
 
-for row in result:
-    print(row)
+def select(username, password):
+    sql = "select * from T_USER where username = '{0}' and password = '{1}'".format(username, password)
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    if len(results) == 0:
+        return False
+    else:
+        return True
+
+def test():
+    username1 = input("请输入用户名：")
+    password1 = input("请输入密码：")
+
+    # 插入数据
+    # insert_sql = "insert into T_USER(id) values (7)"
+    insert_sql = "insert into T_USER(username,password) values ('{0}','{1}')".format(username1, password1)
+    print(insert_sql)
+    cursor.execute(insert_sql)
+    conn.commit();
+
+    # 查询数据
+    sql = "select * from T_USER"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row)
